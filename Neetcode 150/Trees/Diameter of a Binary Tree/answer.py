daf34977bcb427573c44fe1840b0c801572661ee
michaelslice <mjsens523@gmail.com>
2024-10-09 12:11:17 -0500

    Added solutions

--- Neetcode 150/Trees/Diameter of a Binary Tree/answer.py
+++ Neetcode 150/Trees/Diameter of a Binary Tree/answer.py
@@ -1,15 +1,28 @@
 # Definition for a binary tree node.
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        print(root)
-        return 0
-    
+        res = 0
+
+        def dfs(root):
+            nonlocal res
+
+            if root is None:
+                return 0
+            # Recursively find the height of the left and right subtree
+            left = dfs(root.left)
+            right = dfs(root.right)
+            
+            # The sum is the sum of the left and right heights
+            res = max(res, left + right)
+
+            # Return 1 + max, because the height is 1(the node itself)
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return res
+
