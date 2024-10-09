c997f3e920959af7871a979911420f58b4e5e36b
michaelslice <mjsens523@gmail.com>
2024-10-09 13:44:40 -0500

    Added notes

--- Neetcode 150/Trees/Balanced Binary Tree/answer.py
+++ Neetcode 150/Trees/Balanced Binary Tree/answer.py
@@ -4,15 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Note: We return a list of [balanced, height]
+# 
+# left[0]: Wether the left subtree is balanced
+# left[1]: The height of the left subtree 
+# right[0]: Wether the right subtree is balanced
+# right[1]: The height of the left subtree
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
+        # Perform depth first search on the list
         def dfs(root):
+            # Base case if the current node is empty
             if root is None:
                 return [True, 0]
-            
+            # Recursively check the left and right subtree
             left, right = dfs(root.left), dfs(root.right)
+
+            # A tree is balanced if both subtrees and their balanced heights differ by no more than 1
+            # boolean variable
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+            
+            # Returns a tuple
+            # - balanced: Wether the current subtree is balanced
+            # - height: The height of the current subtree, which is 1 + (current node) + max height of the two subtrees
             return [balanced, 1 + max(left[1], right[1])]
 
+        # Call dfs starting from root node
         return dfs(root)[0] 
